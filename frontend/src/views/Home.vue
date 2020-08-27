0f80aa41735b0e1b063bd5f5f8379475d03321d0
Малик Путин <kalik@MacBook-Pro-Malik.local>
2020-08-28 02:07:00 +0400

    Basic empty project structure

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="text-center mt-3">Vue App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+  img {
+    margin: 0 auto;
+    display: block;
+  }
+</style>
